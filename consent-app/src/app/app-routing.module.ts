3e5d3a851bfcae7fa186e5b9bda44783e636211a
Eduardo Magaldi <magaldi1989@gmail.com>
2019-01-26 14:40:13 +0100

    creating pages components and routes

--- consent-app/src/app/app-routing.module.ts
+++ consent-app/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GiveConsentComponent } from './pages/give-consent/give-consent.component';
+import { ConsentsComponent } from './pages/consents/consents.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'give-consent', component: GiveConsentComponent },
+	{ path: '', redirectTo: 'give-consent', pathMatch: 'full' },
+	{ path: 'consents', component: ConsentsComponent },
+	{ path: '404', component: NotFoundComponent },
+	{ path: '**', redirectTo: '404' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- consent-app/src/app/app.module.ts
+++ consent-app/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GiveConsentComponent } from './pages/give-consent/give-consent.component';
+import { ConsentsComponent } from './pages/consents/consents.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		GiveConsentComponent,
+		ConsentsComponent,
+		NotFoundComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
